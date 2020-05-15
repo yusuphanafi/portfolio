ba7974f14df887a61150d86430d302dfdcc2b554
Yusup Hanafi <yhan4fi@gmail.com>
2020-05-15 14:00:00 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// event pada saat link di klik
+$('.page-scroll').on('click', function(e){
+    
+    // ambil isi href
+    var tujuan = $(this).attr('href');
+    // tangkap elemen ybs
+    var elemenTujuan = $(tujuan);
+
+    // pindahkan scroll
+    $('html, body').animate({
+        scrollTop: elemenTujuan.offset().top - 50
+    }, 1250, 'swing');
+
+    e.preventDefault();    
+
+});
